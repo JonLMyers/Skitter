e807c5bba6d897fee30c2f7002f02412a40c9ea1
JonLMyers <jlm4508@g.rit.com>
2016-12-03 12:38:20 -0500

    Super basic beginning to our page.

---
+++ src/main/java/com/skitter/auth/RegisterController.java
@@ -0,0 +1,20 @@
+package com.skitter.auth;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by soren on 12/3/16.
+ */
+@Controller
+public class RegisterController {
+
+    @RequestMapping("/register")
+    public String register(@RequestParam(value="name", required=false, defaultValue = "Jerry") String name, Model model){
+        model.addAttribute("name", name);
+        return "register";
+    }
+
+}
